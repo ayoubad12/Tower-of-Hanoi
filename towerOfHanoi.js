45eee78786b254911ad07355602b9b2f679164af
ayoubad12 <55322505+ayoubad12@users.noreply.github.com>
2021-03-24 03:25:03 +0100

    update the code , it's simpler now because i used the second formula

--- towerOfHanoi.js
+++ towerOfHanoi.js
@@ -1,14 +1,20 @@
 function towerHanoi(discs) {
-	var Current = 1 ;  // the variable "Current" is the number of transitions needed to move 1 disc to the goal (axiom, its the base of my algorithm)
-	var Precedent;
-	
-	if (discs==0){return 0}     //if there is no disc, no transition needed
-	if (discs==1){return 1}     //if there is one disc, you need one transition (axiom)
-	if (discs>1){               //for more than 1 disc 
-			for (i=2; i<=discs; i++){
-				Precedent = Current ;
-				Current = 2*Precedent+1 ;
-			}
-		return Current ;
-	}
+	if(discs>=0){return 2**discs-1}
 }
+
+
+
+// if you didn't use the second formula you would have to do all this :
+ 
+// var Current = 1 ;  // the variable "Current" is the number of transitions needed to move 1 disc to the goal (axiom, its the base of my algorithm)
+	// var Precedent;
+	
+	// if (discs==0){return 0}     //if there is no disc, no transition needed
+	// if (discs==1){return 1}     //if there is one disc, you need one transition (axiom)
+	// if (discs>1){               //for more than 1 disc 
+	// 		for (i=2; i<=discs; i++){
+	// 			Precedent = Current ;
+	// 			Current = 2*Precedent+1 ;
+	// 		}
+	// 	return Current ;
+	// }
